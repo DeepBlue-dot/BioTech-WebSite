115fece9cbe9416b2dd1b5ae1d72db15cce4dfbd
DeepBlue <deepblue5991@gmail.com>
2024-05-05 14:48:52 +0300

    Update index.html, script.js, and style.css

--- script/script.js
+++ script/script.js
@@ -1,20 +1,10 @@
 function openNav() {
     document.getElementById("sideMenu")
-        .style.width = "300px";
-    document.getElementById("contentArea")
-        .style.marginLeft = "300px";
+        .style.width = "250px";
 }
  
 function closeNav() {
     document.getElementById("sideMenu")
         .style.width = "0";
-    document.getElementById("contentArea")
-        .style.marginLeft = "0";
 }
  
-function showContent(content) {
-    document.getElementById("contentTitle")
-        .textContent = content + " page";
-         
-    closeNav();
-}
