e9710102364c491fe34258edff01b64817548efa
DeepBlue <deepblue5991@gmail.com>
2024-05-07 00:34:00 +0300

    Update sidebar functionality and styles

--- script/script.js
+++ script/script.js
@@ -1,10 +1,27 @@
 function openNav() {
-    document.getElementById("sideMenu")
-        .style.width = "275px";
+    // Open sidebar
+    document.getElementById("sideMenu").style.width = "275px";
+
+    // Create and append foreground overlay
+    var overlay = document.createElement("div");
+    overlay.id = "overlay";
+    overlay.style.position = "fixed";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Adjust opacity as needed
+    overlay.style.zIndex = "2"; // Ensure the overlay appears above other content
+    document.body.appendChild(overlay);
 }
- 
+
 function closeNav() {
-    document.getElementById("sideMenu")
-        .style.width = "0";
+    // Close sidebar
+    document.getElementById("sideMenu").style.width = "0";
+
+    // Remove foreground overlay
+    var overlay = document.getElementById("overlay");
+    if (overlay) {
+        overlay.parentNode.removeChild(overlay);
+    }
 }
- 
